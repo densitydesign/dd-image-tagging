c0dd7b36dbb40c6be49b2a7346c20889ced75d99
Mitch90 <Mitch90@users.noreply.github.com>
2019-01-16 13:42:57 +0100

    fix models and loading

--- js/csv-worker.js
+++ js/csv-worker.js
@@ -1,22 +1,25 @@
-onmessage = function(e) {
+onmessage = function (e) {
 
     let conceptsArray = [];
 
     for (let i = 0; i < e.data['response'].length; i++) {
         let output = e.data['response'][i];
         let imageUrl = output['input']['data']['image']['url'];
-
+        
         if (e.data['model']['givesConcepts']) {
             if (Object.keys(output['data']).length === 0 && output['data'].constructor === Object) {
                 conceptsArray.push({
                     url: imageUrl,
-                    concept: null,
+                    concept: 'No elements present',
                     confidence: null
                 });
             } else {
+                let maxConfidence = 0;
+
                 for (let j = 0; j < output['data']['concepts'].length; j++) {
                     let concept = output['data']['concepts'][j]['name'];
                     let confidence = output['data']['concepts'][j]['value'];
+                    maxConfidence = Math.max(maxConfidence, confidence);
 
                     if (+confidence >= 0.1) {
                         conceptsArray.push({
@@ -25,6 +28,14 @@ onmessage = function(e) {
                             confidence: +confidence
                         });
                     }
+                }
+
+                if (maxConfidence < 0.1) {
+                    conceptsArray.push({
+                        url: imageUrl,
+                        concept: 'No elements present',
+                        confidence: null
+                    });
                 }
             }
 
@@ -36,26 +47,45 @@ onmessage = function(e) {
                         conceptsArray.push({
                             url: imageUrl,
                             celebrity: 'No celebrity present',
-                            bbox: null,
-                            confidence: null
+                            confidence: null,
+                            bbox_border_top: null,
+                            bbox_border_bottom: null,
+                            bbox_border_left: null,
+                            bbox_border_right: null
                         });
                     } else {
                         for (let j = 0; j < output['data']['regions'].length; j++) {
                             let regions = output['data']['regions'][j];
                             let bbox = regions['region_info']['bounding_box'];
+                            let maxConfidence = 0;
 
                             for (let k = 0; k < regions['data']['face']['identity']['concepts'].length; k++) {
                                 let celebrity = regions['data']['face']['identity']['concepts'][k]['name'];
                                 let confidence = regions['data']['face']['identity']['concepts'][k]['value'];
+                                maxConfidence = Math.max(maxConfidence, confidence);
 
                                 if (+confidence >= 0.1) {
                                     conceptsArray.push({
                                         url: imageUrl,
                                         celebrity: celebrity,
-                                        bbox: bbox,
-                                        confidence: +confidence
+                                        confidence: +confidence,
+                                        bbox_border_top: Math.round(bbox.top_row * 100) + '%',
+                                        bbox_border_bottom: Math.round(bbox.bottom_row * 100) + '%',
+                                        bbox_border_left: Math.round(bbox.left_col * 100) + '%',
+                                        bbox_border_right: Math.round(bbox.right_col * 100) + '%'
                                     });
                                 }
+                            }
+                            if (maxConfidence < 0.1) {
+                                conceptsArray.push({
+                                    url: imageUrl,
+                                    celebrity: 'No celebrity present',
+                                    confidence: null,
+                                    bbox_border_top: null,
+                                    bbox_border_bottom: null,
+                                    bbox_border_left: null,
+                                    bbox_border_right: null
+                                });
                             }
                         }
                     }
@@ -65,10 +95,12 @@ onmessage = function(e) {
                     for (let j = 0; j < output['data']['colors'].length; j++) {
                         let color = output['data']['colors'][j]['raw_hex'];
                         let density = output['data']['colors'][j]['value'];
+                        let colorName = output['data']['colors'][j]['w3c']['name'];
 
                         conceptsArray.push({
                             url: imageUrl,
-                            color: color,
+                            name: colorName,
+                            hex: color,
                             density: +density
                         });
                     }
@@ -78,10 +110,14 @@ onmessage = function(e) {
                     if (Object.keys(output['data']).length === 0 && output['data'].constructor === Object) {
                         conceptsArray.push({
                             url: imageUrl,
+                            person_present: false,
                             age: null,
                             gender: null,
                             multicultural: null,
-                            bbox: null,
+                            bbox_border_top: null,
+                            bbox_border_bottom: null,
+                            bbox_border_left: null,
+                            bbox_border_right: null
                         });
                     } else {
                         for (let j = 0; j < output['data']['regions'].length; j++) {
@@ -122,15 +158,33 @@ onmessage = function(e) {
                                 }
                             }
 
-                            age = Math.round(mean / factors);
+                            if (mean == 0) {
+                                conceptsArray.push({
+                                    url: imageUrl,
+                                    person_present: false,
+                                    age: null,
+                                    gender: null,
+                                    multicultural: null,
+                                    bbox_border_top: null,
+                                    bbox_border_bottom: null,
+                                    bbox_border_left: null,
+                                    bbox_border_right: null
+                                });
+                            } else {
+                                age = Math.round(mean / factors);
 
-                            conceptsArray.push({
-                                url: imageUrl,
-                                age: age,
-                                gender: gender,
-                                multicultural: multicultural,
-                                bbox: bbox,
-                            });
+                                conceptsArray.push({
+                                    url: imageUrl,
+                                    person_present: true,
+                                    age: age,
+                                    gender: gender,
+                                    multicultural: multicultural,
+                                    bbox_border_top: Math.round(bbox.top_row * 100) + '%',
+                                    bbox_border_bottom: Math.round(bbox.bottom_row * 100) + '%',
+                                    bbox_border_left: Math.round(bbox.left_col * 100) + '%',
+                                    bbox_border_right: Math.round(bbox.right_col * 100) + '%'
+                                });
+                            }
                         }
                     }
                     break;
@@ -139,7 +193,10 @@ onmessage = function(e) {
                     if (Object.keys(output['data']).length === 0 && output['data'].constructor === Object) {
                         conceptsArray.push({
                             url: imageUrl,
-                            bbox: null
+                            bbox_border_top: null,
+                            bbox_border_bottom: null,
+                            bbox_border_left: null,
+                            bbox_border_right: null
                         });
                     } else {
                         for (let j = 0; j < output['data']['regions'].length; j++) {
@@ -148,7 +205,10 @@ onmessage = function(e) {
 
                             conceptsArray.push({
                                 url: imageUrl,
-                                bbox: bbox
+                                bbox_border_top: Math.round(bbox.top_row * 100) + '%',
+                                bbox_border_bottom: Math.round(bbox.bottom_row * 100) + '%',
+                                bbox_border_left: Math.round(bbox.left_col * 100) + '%',
+                                bbox_border_right: Math.round(bbox.right_col * 100) + '%'
                             });
                         }
                     }
